2838d3a1b792095d1e29b7d6a0c1e025a78bce42
tjwj123 <770272091@qq.com>
2021-10-21 10:40:42 +0800

    1021

--- src/main/java/com/example/demo/MsTest.java
+++ src/main/java/com/example/demo/MsTest.java
@@ -277,6 +277,22 @@ public class MsTest {
         return count;
     }
 
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        for (int i = len - 1; i >= 0; ++i) {
+            if (digits[i] != 9) {
+                ++digits[i];
+                for (int j = i + 1; j < len; ++j) {
+                    digits[j] = 0;
+                }
+                return digits;
+            }
+        }
+        int[] res = new int[len];
+        res[0] = 1;
+        return res;
+    }
+
 
     public int jump(int[] nums) {
         int len = nums.length;
